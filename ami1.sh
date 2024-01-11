b399e7ba73a9da92a7a0fea9e6b9319ef520ddac
Ubuntu <amith613@gitlinux.hezk2jvhtyjexnvdkqifod0yvh.rx.internal.cloudapp.net>
2024-01-11 19:02:32 +0000

    my first shell script

---
+++ ami1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "hello world"
+echo "enter the 2 numbers"
+echo "enter number 1"
+read a
+echo "enter number 2"
+read b
+c=$((a + b))
+echo "sum of 2 numbers $c"
+
+echo "adding extra line"
+
